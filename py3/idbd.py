45037ce77c9cf9c892909528c5b9e985f0f16088
rldotai <babennett@protonmail.com>
2016-07-07 11:51:10 -0600

    Added initial version of IDBD algorithm

---
+++ py3/idbd.py
@@ -0,0 +1,42 @@
+"""
+Incremental Delta-Bar-Delta (IDBD), a stepsize adjustment algorithm.
+
+"Adapting Bias by Gradient Descent: An Incremental Version of Delta-Bar-Delta", 
+Sutton, 92
+"""
+
+class IDBD:
+    """
+    Incremental Delta-Bar-Delta, or IDBD.
+
+    Attributes
+    ----------
+    n : int 
+
+    alpha : Vector[float]
+        The vector of per-weight stepsizes.
+    beta : Vector[float]
+        The vector of logarithmic per-weight stepsizes.
+    w : Vector[float]
+        Weight vector.
+    h : Vector[float]
+        Update memory trace.
+    eta : float 
+        Meta stepsize parameter.
+    """
+    def __init__(self, n, eta=1):
+        self.n = n 
+        self.eta = eta
+        self.reset()
+
+    def reset(self):
+        # What should beta be initialized to? Should `w` be zeros or random?
+        self.beta = (-1/self.n)*np.ones(self.n)
+        self.h = np.zeros(self.n)
+        self.w = np.zeros(self.n)
+
+    def update(self, x, delta):
+        self.beta += self.eta * self.h * delta * x
+        self.alpha = np.exp(self.beta)
+        self.w += self.alpha * delta * x 
+        self.h = self.h * np.max(0, 1 - self.alpha * x**2) + self.alpha*delta*x
